6579d72c1e4fef092c182a1372b5612cab72383c
Aleksandar Stanković <sa190478d@student.etf.bg.ac.rs>
2023-05-25 14:52:30 +0200

    Aleksandar: ispravljen skript kako bi bile ispoštovane zavisnosti tabela

--- Faza4/THE BOYS Skript za pravljenje baze podataka.sql
+++ Faza4/THE BOYS Skript za pravljenje baze podataka.sql
@@ -1,20 +1,5 @@
-CREATE TABLE `zauzeto_sediste` (
-  `zauzetoSedisteId` int NOT NULL AUTO_INCREMENT,
-  `projekcijaId` int NOT NULL,
-  `sediste` int NOT NULL,
-  PRIMARY KEY (`zauzetoSedisteId`),
-  UNIQUE KEY `zauzetoSedisteId_UNIQUE` (`zauzetoSedisteId`),
-  KEY `zauzeto_sediste_projekcijaId_idx` (`projekcijaId`),
-  CONSTRAINT `zauzeto_sediste_projekcijaId` FOREIGN KEY (`projekcijaId`) REFERENCES `projekcija` (`projekcijaId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
-CREATE TABLE `bioskop` (
-  `bioskopId` int NOT NULL AUTO_INCREMENT,
-  `ime` varchar(45) NOT NULL,
-  PRIMARY KEY (`bioskopId`),
-  UNIQUE KEY `bioskopId_UNIQUE` (`bioskopId`),
-  UNIQUE KEY `ime_UNIQUE` (`ime`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+CREATE DATABASE cinemax;
+USE cinemax;
 
 CREATE TABLE `film` (
   `filmId` int NOT NULL AUTO_INCREMENT,
@@ -30,24 +15,58 @@ CREATE TABLE `film` (
   UNIQUE KEY `filmId_UNIQUE` (`filmId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE `format` (
-  `formatId` int NOT NULL AUTO_INCREMENT,
+CREATE TABLE `zanr` (
+  `zanrId` int NOT NULL AUTO_INCREMENT,
   `ime` varchar(20) NOT NULL,
-  PRIMARY KEY (`formatId`),
-  UNIQUE KEY `formatId_UNIQUE` (`formatId`),
-  UNIQUE KEY `ime_UNIQUE` (`ime`)
+  PRIMARY KEY (`zanrId`),
+  UNIQUE KEY `zanrId_UNIQUE` (`zanrId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE `format_sala` (
-  `formatSalaId` int NOT NULL AUTO_INCREMENT,
-  `formatId` int NOT NULL,
-  `salaId` int NOT NULL,
-  PRIMARY KEY (`formatSalaId`),
-  UNIQUE KEY `formatSalaId_UNIQUE` (`formatSalaId`),
-  KEY `format_sala_formatId_idx` (`formatId`),
-  KEY `format_sala_salaId_idx` (`salaId`),
-  CONSTRAINT `format_sala_formatId` FOREIGN KEY (`formatId`) REFERENCES `format` (`formatId`),
-  CONSTRAINT `format_sala_salaId` FOREIGN KEY (`salaId`) REFERENCES `sala` (`salaId`)
+CREATE TABLE `zanr_film` (
+  `zanrFilmId` int NOT NULL AUTO_INCREMENT,
+  `zanrId` int NOT NULL,
+  `filmId` int NOT NULL,
+  PRIMARY KEY (`zanrFilmId`),
+  UNIQUE KEY `zanrFilmId_UNIQUE` (`zanrFilmId`),
+  KEY `zanr_film_zanrId_idx` (`zanrId`),
+  KEY `zanr_film_filmId_idx` (`filmId`),
+  CONSTRAINT `zanr_film_filmId` FOREIGN KEY (`filmId`) REFERENCES `film` (`filmId`),
+  CONSTRAINT `zanr_film_zanrId` FOREIGN KEY (`zanrId`) REFERENCES `zanr` (`zanrId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `slika_film` (
+  `slikaFilmId` int NOT NULL AUTO_INCREMENT,
+  `slika` longblob NOT NULL,
+  `filmId` int NOT NULL,
+  PRIMARY KEY (`slikaFilmId`),
+  UNIQUE KEY `slikaFilmId_UNIQUE` (`slikaFilmId`),
+  KEY `filmId_idx` (`filmId`),
+  CONSTRAINT `slika_film_filmId` FOREIGN KEY (`filmId`) REFERENCES `film` (`filmId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `proizvod` (
+  `proizvodId` int NOT NULL AUTO_INCREMENT,
+  `ime` varchar(45) NOT NULL,
+  `filmId` int NOT NULL,
+  `tip` varchar(10) NOT NULL,
+  `opis` varchar(300) NOT NULL,
+  `dinari` int NOT NULL,
+  `bodovi` int NOT NULL,
+  `kolicina` int NOT NULL,
+  PRIMARY KEY (`proizvodId`),
+  UNIQUE KEY `proizvodId_UNIQUE` (`proizvodId`),
+  KEY `filmId_idx` (`filmId`),
+  CONSTRAINT `proizvod_filmId` FOREIGN KEY (`filmId`) REFERENCES `film` (`filmId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `slika_proizvod` (
+  `slikaProizvodId` int NOT NULL AUTO_INCREMENT,
+  `slika` longblob NOT NULL,
+  `proizvodId` int NOT NULL,
+  PRIMARY KEY (`slikaProizvodId`),
+  UNIQUE KEY `proizvodSlikaId_UNIQUE` (`slikaProizvodId`),
+  KEY `proizvod_slika_proizvodId_idx` (`proizvodId`),
+  CONSTRAINT `slika_proizvod_proizvodId` FOREIGN KEY (`proizvodId`) REFERENCES `proizvod` (`proizvodId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE `korisnik` (
@@ -93,19 +112,43 @@ CREATE TABLE `narudzbina` (
   CONSTRAINT `narudzbina_proizvodId` FOREIGN KEY (`proizvodId`) REFERENCES `proizvod` (`proizvodId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE `proizvod` (
-  `proizvodId` int NOT NULL AUTO_INCREMENT,
+CREATE TABLE `format` (
+  `formatId` int NOT NULL AUTO_INCREMENT,
+  `ime` varchar(20) NOT NULL,
+  PRIMARY KEY (`formatId`),
+  UNIQUE KEY `formatId_UNIQUE` (`formatId`),
+  UNIQUE KEY `ime_UNIQUE` (`ime`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `bioskop` (
+  `bioskopId` int NOT NULL AUTO_INCREMENT,
   `ime` varchar(45) NOT NULL,
-  `filmId` int NOT NULL,
-  `tip` varchar(10) NOT NULL,
-  `opis` varchar(300) NOT NULL,
-  `dinari` int NOT NULL,
-  `bodovi` int NOT NULL,
-  `kolicina` int NOT NULL,
-  PRIMARY KEY (`proizvodId`),
-  UNIQUE KEY `proizvodId_UNIQUE` (`proizvodId`),
-  KEY `filmId_idx` (`filmId`),
-  CONSTRAINT `proizvod_filmId` FOREIGN KEY (`filmId`) REFERENCES `film` (`filmId`)
+  PRIMARY KEY (`bioskopId`),
+  UNIQUE KEY `bioskopId_UNIQUE` (`bioskopId`),
+  UNIQUE KEY `ime_UNIQUE` (`ime`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `sala` (
+  `salaId` int NOT NULL AUTO_INCREMENT,
+  `broj` int NOT NULL,
+  `bioskopId` int NOT NULL,
+  `brojSedista` int NOT NULL,
+  PRIMARY KEY (`salaId`),
+  UNIQUE KEY `salaId_UNIQUE` (`salaId`),
+  KEY `bioskopId_idx` (`bioskopId`),
+  CONSTRAINT `sala_bioskopId` FOREIGN KEY (`bioskopId`) REFERENCES `bioskop` (`bioskopId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `format_sala` (
+  `formatSalaId` int NOT NULL AUTO_INCREMENT,
+  `formatId` int NOT NULL,
+  `salaId` int NOT NULL,
+  PRIMARY KEY (`formatSalaId`),
+  UNIQUE KEY `formatSalaId_UNIQUE` (`formatSalaId`),
+  KEY `format_sala_formatId_idx` (`formatId`),
+  KEY `format_sala_salaId_idx` (`salaId`),
+  CONSTRAINT `format_sala_formatId` FOREIGN KEY (`formatId`) REFERENCES `format` (`formatId`),
+  CONSTRAINT `format_sala_salaId` FOREIGN KEY (`salaId`) REFERENCES `sala` (`salaId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE `projekcija` (
@@ -129,6 +172,16 @@ CREATE TABLE `projekcija` (
   CONSTRAINT `projekcija_salaId` FOREIGN KEY (`salaId`) REFERENCES `sala` (`salaId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
+CREATE TABLE `zauzeto_sediste` (
+  `zauzetoSedisteId` int NOT NULL AUTO_INCREMENT,
+  `projekcijaId` int NOT NULL,
+  `sediste` int NOT NULL,
+  PRIMARY KEY (`zauzetoSedisteId`),
+  UNIQUE KEY `zauzetoSedisteId_UNIQUE` (`zauzetoSedisteId`),
+  KEY `zauzeto_sediste_projekcijaId_idx` (`projekcijaId`),
+  CONSTRAINT `zauzeto_sediste_projekcijaId` FOREIGN KEY (`projekcijaId`) REFERENCES `projekcija` (`projekcijaId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
 CREATE TABLE `rezervacija` (
   `rezervacijaId` int NOT NULL AUTO_INCREMENT,
   `projekcijaId` int NOT NULL,
@@ -142,52 +195,4 @@ CREATE TABLE `rezervacija` (
   CONSTRAINT `rezervacija_projekcijaId` FOREIGN KEY (`projekcijaId`) REFERENCES `projekcija` (`projekcijaId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE `sala` (
-  `salaId` int NOT NULL AUTO_INCREMENT,
-  `broj` int NOT NULL,
-  `bioskopId` int NOT NULL,
-  `brojSedista` int NOT NULL,
-  PRIMARY KEY (`salaId`),
-  UNIQUE KEY `salaId_UNIQUE` (`salaId`),
-  KEY `bioskopId_idx` (`bioskopId`),
-  CONSTRAINT `sala_bioskopId` FOREIGN KEY (`bioskopId`) REFERENCES `bioskop` (`bioskopId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
-CREATE TABLE `slika_film` (
-  `slikaFilmId` int NOT NULL AUTO_INCREMENT,
-  `slika` longblob NOT NULL,
-  `filmId` int NOT NULL,
-  PRIMARY KEY (`slikaFilmId`),
-  UNIQUE KEY `slikaFilmId_UNIQUE` (`slikaFilmId`),
-  KEY `filmId_idx` (`filmId`),
-  CONSTRAINT `slika_film_filmId` FOREIGN KEY (`filmId`) REFERENCES `film` (`filmId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
-CREATE TABLE `slika_proizvod` (
-  `slikaProizvodId` int NOT NULL AUTO_INCREMENT,
-  `slika` longblob NOT NULL,
-  `proizvodId` int NOT NULL,
-  PRIMARY KEY (`slikaProizvodId`),
-  UNIQUE KEY `proizvodSlikaId_UNIQUE` (`slikaProizvodId`),
-  KEY `proizvod_slika_proizvodId_idx` (`proizvodId`),
-  CONSTRAINT `slika_proizvod_proizvodId` FOREIGN KEY (`proizvodId`) REFERENCES `proizvod` (`proizvodId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
-CREATE TABLE `zanr` (
-  `zanrId` int NOT NULL AUTO_INCREMENT,
-  `ime` varchar(20) NOT NULL,
-  PRIMARY KEY (`zanrId`),
-  UNIQUE KEY `zanrId_UNIQUE` (`zanrId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
-CREATE TABLE `zanr_film` (
-  `zanrFilmId` int NOT NULL AUTO_INCREMENT,
-  `zanrId` int NOT NULL,
-  `filmId` int NOT NULL,
-  PRIMARY KEY (`zanrFilmId`),
-  UNIQUE KEY `zanrFilmId_UNIQUE` (`zanrFilmId`),
-  KEY `zanr_film_zanrId_idx` (`zanrId`),
-  KEY `zanr_film_filmId_idx` (`filmId`),
-  CONSTRAINT `zanr_film_filmId` FOREIGN KEY (`filmId`) REFERENCES `film` (`filmId`),
-  CONSTRAINT `zanr_film_zanrId` FOREIGN KEY (`zanrId`) REFERENCES `zanr` (`zanrId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+INSERT INTO korisnik (email, korisnickoIme, ime, prezime, sifra, kategorija, bodovi) VALUES ("admin@admin.com", "admin", "Aleksandar", "Stanković", "123", "a", 0);
